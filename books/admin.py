5c50dce999505177c56a037b7015efe2600c48f9
Blue <blue@Blues-iMac.local>
2022-11-08 14:36:40 +0100

    fixed index

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, Books, Publishing_house, Translater, Genre
+from .models import Author, Books, Publishing_house, Translater, Genre, Client
 
 # Register your models here.
 
@@ -13,13 +13,17 @@ class TranslaterAdmin(admin.ModelAdmin):
     list_display = ("id", "first_name", "last_name")
 
 class Publishing_house_Admin(admin.ModelAdmin):
-    list_display = ("id", "publishing_house_name", "manager_first_name", "manager_last_name", "manager_contact_number", "manager_email")
+    list_display = ("id", "publishing_house_name", "manager_first_name", "manager_last_name", "manager_email")
 
 class BooksAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "price", "number_in_stock")
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("name", "surname", "birthday", "email")
 
 admin.site.register(Books, BooksAdmin)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Translater, TranslaterAdmin)
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Publishing_house, Publishing_house_Admin)
+admin.site.register(Client, ClientAdmin)
--- books/models.py
+++ books/models.py
@@ -28,21 +28,34 @@ class Publishing_house(models.Model):
     publishing_house_name = models.CharField(max_length=255)
     manager_first_name = models.CharField(max_length=255)
     manager_last_name = models.CharField(max_length=255)
-    manager_contact_number = models.IntegerField()
+    # manager_contact_number = models.IntegerField()
     manager_email = models.EmailField()
 
     def __str__(self):
-        return "'%s', '%s', '%s', '%s', '%s'" % (self.publishing_house_name, self.manager_first_name, self.manager_last_name, self.manager_contact_number, self.manager_email)
+        return "'%s', '%s', '%s', '%s', '%s'" % (self.publishing_house_name, self.manager_first_name, self.manager_last_name, self.manager_email)
 
 class Books(models.Model):
     name = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
-    price = models.IntegerField()
+    price = models.CharField(max_length = 255)
     number_in_stock = models.IntegerField()
-    isbn = models.CharField(max_length=40)
+    # isbn = models.CharField(max_length=255)
     translater = models.ForeignKey(Translater, on_delete=models.CASCADE)
     publishing_house = models.ForeignKey(Publishing_house, on_delete=models.CASCADE)
 
     def __str__(self):
         return "%s %s %s" % (self.name, self.price, self.number_in_stock)
+
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    birthday = models.DateField()
+    # phone_number = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return "%s %s %s %s %s" % (self.name, self.surname, self.birthday, self.email)
+
+class Staff(models.Model):
+    name = models.CharField(max_length=255)
